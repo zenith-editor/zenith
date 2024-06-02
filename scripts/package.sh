9d9c8150fd453c64bea7ec53a5f692f4c571e961
T. M <pm2mtr@gmail.com>
2024-06-02 01:46:47 +0000

    update build script to make it reproducible

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,8 +8,12 @@ fi
 
 package_temp=$(mktemp -d || exit 1)
 
-mkdir $package_temp/zig-out
-mkdir $package_temp/extra
+SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+
+git clone . $package_temp/src
+
+mkdir -p $package_temp/src/{zig-cache,zig-out}
+mkdir -p $package_temp/extra
 
 cp -r ./docs $package_temp/docs
 cp ./LICENSE $package_temp/docs
@@ -20,25 +24,31 @@ cleanup() {
   rm -rf $package_temp
 }
 
-[[ -L "./zig-cache" ]] && rm zig-cache && mkdir zig-cache
-
 docker run \
   -v $zig_path:/zig \
-  -v .:/app \
-  --tmpfs /app/zig-cache:size=512m \
-  -v $package_temp/zig-out:/app/zig-out \
-  -v $package_temp/extra:/extra \
-  -it alpine sh -c "apk add git && echo \`uname -s\`-\`uname -m\` > /extra/arch && cd /app && /zig/zig build  -Doptimize=ReleaseSafe"
+  -v $package_temp:/app \
+  -it alpine sh -c "apk add git && \
+    echo \`uname -s\`-\`uname -m\` > /app/extra/arch && \
+    cd /app/src && /zig/zig build -Doptimize=ReleaseSafe"
 if [ $? -ne 0 ]; then
   cleanup; exit 1
 fi
 
 arch=$(cat $package_temp/extra/arch)
 version=$(git describe --tags)
-mv $package_temp/zig-out/bin/zenith $package_temp/zenith
-old_pwd=$(pwd)
+archive_name=zenith-$version-$arch.tar.gz 
+
+mv $package_temp/src/zig-out/bin/zenith $package_temp/zenith
+output_dir=$(pwd)/packages/
+mkdir -p $output_dir
 cd $package_temp
-tar -cvzf $old_pwd/zenith-$version-$arch.tar.gz ./docs ./zenith
-cd $old_pwd
+tar \
+      --sort=name \
+      --mtime="@${SOURCE_DATE_EPOCH}" \
+      --owner=0 --group=0 --numeric-owner \
+      --format=ustar \
+      -cvzf $output_dir/$archive_name ./docs ./zenith
+cd $output_dir
+sha256sum $archive_name > SHA256SUMS
 
 cleanup
